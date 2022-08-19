28179bfd936123c443a1be68e2170e54953d87a6
nyawoi <106277673+nyawoi@users.noreply.github.com>
2022-08-19 13:53:05 -0500

    Fixed issue with Sundial not resetting day length to default

---
+++ Sundial/Hooks/SundialCleanupHook.cs
@@ -0,0 +1,14 @@
+﻿using AetharNet.Moonbow.Experimental.Templates;
+using Staxel.Core;
+using Staxel.Modding;
+
+namespace AetharNet.Sundial.Hooks
+{
+    internal class SundialCleanupHook : ModHookV4Template, IModHookV4
+    {
+        public override void CleanupOldSession()
+        {
+            Constants.SecondsPerIngameDay = 1320;
+        }
+    }
+}
